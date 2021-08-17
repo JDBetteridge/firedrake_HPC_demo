f4c412ef79335c844e2132ce81e2cca9b8a223e5
JDBetteridge <j.betteridge@imperial.ac.uk>
2021-08-17 15:06:50 +0100

    Added warm up repeat and skips sims that took more than 2mins on prev iteration

--- singlecore.py
+++ singlecore.py
@@ -4,19 +4,19 @@ from time import time
 import gc
 gc.disable()
 
-baseN_list = [round(4*2**(ii/3)) for ii in range(7)]
+baseN_list = [4] + [round(4*2**(ii/3)) for ii in range(7)]
 nref = 2
 degree = 2
 
-baseN_list = [16]
-
 csvfile = ResultsCSV('singlecore.csv')
 
+maxtime = 120
+skip = []
 for baseN in baseN_list:
     problem, u_h, truth = make_problem(baseN, nref, degree)
     for key, value in solver_dict.items():
-        if (baseN > 13) and (key == 'LU'):
-            parprint('BaseN:', baseN, 'too big for LU, skipping')
+        if key in skip:
+            parprint(f'BaseN: {baseN} too big for {key}, skipping')
             continue
 
         u_h.assign(0)
@@ -25,7 +25,10 @@ for baseN in baseN_list:
         solver.solve()
         t = time() - t
 
+        if t > maxtime:
+            skip.append(key)
+
         recerror = errornorm(truth, u_h)
         dofs = u_h.function_space().dim()
         csvfile.record_result(baseN, nref, degree, key, recerror, t, dofs)
-        parprint(f'BaseN: {baseN:3d} Solver: {key:25} Error: {recerror:8.5f} Time: {t:8.5f} s')
+        parprint(f'BaseN: {baseN:3d} Solver: {key:35} Error: {recerror:8.5e} Time: {t:8.5f} s')
